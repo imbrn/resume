7e418d95c568f4139164c07e73a0cb9ff4756e2c
Bruno C. Couto <brunodev02221@gmail.com>
2018-01-16 13:13:41 -0200

    Add support to ES6 and CSS
    Add babel transpilation and PostCSS.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: "./main.js",
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules)/,
+        loader: ["style-loader", "css-loader", "postcss-loader"]
+      }
+    ]
+  }
+};
