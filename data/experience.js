a42a2c9c0b7f69ee597bf413b79915d1231bd943
Bruno C. Couto <brunodev02221@gmail.com>
2018-01-18 15:44:07 -0200

    Update data and design
    Update experience projects description and change the clean theme
    primary color to green.

--- data/experience.js
+++ data/experience.js
@@ -3,28 +3,28 @@ export default {
     {
       project: "Open flashcards",
       role: "Full stack engineer",
-      description: "Open flashcards is an open source project for studying flashcards. But it's also a project focused in helping others to understand and participate in a real project development. The mainly used technologies are React, Redux and Firebase.",
+      description: "Open flashcards is an open source project for studying flashcards. But it's also a project focused on helping others to understand and participate in a real project development. The mainly used technologies are React, Redux and Firebase.",
       period: "In progress"
     },
     {
       project: "Awesome React Steps",
       role: "Front-end developer",
       description:
-        "Reusable and customizable set of open source react steppers components. This library was built using ES6 (transpiling with babel), PostCSS, and was packaged using Rollup. I developed the entirely library by myself.",
+        "Open source reusable and customizable set of React steppers components. This library was built using ES6 (transpile with babel), PostCSS, and was packaged using Rollup. I developed the entire library by myself.",
       period: "2017-2018"
     },
     {
       project: "Magic Matrix Puzzle",
       role: "Game developer",
       description:
-        "Android game in Rubik's cube style, but in a 2D space. This game was built using Cocos2d-X as game engine, JNI was used to connect Java code with C++ code. I engineered and developed the entirely game by myself.",
+        "An android game in Rubik's cube style, but in a 2D space. This game was built using Cocos2d-X as the game engine, JNI was used to connect Java code with C++ code. I engineered and developed the entire game by myself.",
       period: "2015-2016"
     },
     {
       project: "Slide to Contrary Side",
       role: "Game developer",
       description:
-        "Android arcade, one touch game. This game was built using Cocos2d-X as game engine, JNI was used to connect Java code with C++ code. I engineered and developed the entirely game by myself.",
+        "Android arcade, one-touch game. This game was built using Cocos2d-X as the game engine, JNI was used to connect Java code with C++ code. I engineered and developed the entire game by myself.",
       period: "2015"
     }
   ]
