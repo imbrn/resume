3752abf5bf14851173fd28665972437760bf24ec
Bruno C. Couto <brunodev02221@gmail.com>
2018-02-20 15:54:18 -0300

    Lines break in data files

--- data/education.js
+++ data/education.js
@@ -4,13 +4,16 @@ export default {
       "title": "Self-taught CS",
       "institution": "The web",
       "period": "2008-forever",
-      "description": "Self-taught computer science using web for research, taking free courses at schools like Udacity and Coursera, coding challenges, reading books, watching videos, and everything awesome from the OSS community."
+      "description": `Self-taught computer science using web for research,
+        taking free courses at schools like Udacity and Coursera,
+        coding challenges, reading books, watching videos, and everything
+        awesome from the OSS community.`
     },
     {
       "title": "Information systems",
       "institution": "FAFRAM",
       "period": "2006-2008 (dropped out)",
-      "description": "Analysis and development of systems."
+      "description": `Analysis and development of systems.`
     }
   ],
   pt: [
@@ -18,7 +21,10 @@ export default {
       "title": "Auto-didata CC.",
       "institution": "The web",
       "period": "2008-forever",
-      "description": "Auto-didata em ciências da computação utilizando a web como ferramenta de pesquisa, realizando cursos em escolas como Udacity e Coursera, participando de desafios de programação, lendo livros, assistindo vídeos, e tudo que tem de melhor na comunidade open-source."
+      "description": `Auto-didata em ciências da computação utilizando a web
+        como ferramenta de pesquisa, realizando cursos em escolas como Udacity
+        e Coursera, participando de desafios de programação, lendo livros,
+        assistindo vídeos, e tudo que tem de melhor na comunidade open-source.`
     },
     {
       "title": "Sistemas de informação",
